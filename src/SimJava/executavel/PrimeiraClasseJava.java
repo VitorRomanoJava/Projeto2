1e74a98e479855e91be9286f19347e153d89eaaf
vitor romano pena <vitor romano pena@DESKTOP-8GG66H5>
2023-07-14 14:35:25 -0300

    Criação do projeto

---
+++ src/SimJava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package SimJava.executavel;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+		/*Main é um metodo auto executavel em Java*/
+		
+		System.out.println("Olá mundo");
+		/*Escreve texto no console*/ 
+		
+	}
+}
